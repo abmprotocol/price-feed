487e3eb73edb98712eff4731f3c4cd849d5b2a8f
abmprotocol <abmprotocol@gmail.com>
2021-02-26 20:03:11 +0000

    commit

--- src/feed-factory.sol
+++ src/feed-factory.sol
@@ -7,10 +7,9 @@ contract FeedFactory {
     mapping(address=>bool) public isFeed;
 
     function create() public returns (PriceFeed) {
-        PriceFeed feed = new PriceFeed();
-        emit Created(msg.sender, address(feed));
-        feed.setOwner(msg.sender);
+        address feed = new PriceFeed();
+        emit Created(msg.sender, feed);
+        PriceFeed(feed).setOwner(msg.sender);
         isFeed[feed] = true;
-        return feed;
-    }
+        return PriceFeed(feed);
 }
